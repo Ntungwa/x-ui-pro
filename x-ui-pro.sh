73b1d20783e94669d0967d15f0da3ebdebf81060
Zeno <86835361+Ntungwa@users.noreply.github.com>
2025-04-05 23:11:43 +0200

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -369,6 +369,7 @@ server {
                 grpc_set_header Host $host;
                 grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
                 grpc_pass grpc://127.0.0.1:$fwdport;
+		break;
         }
 	#Xray Config Path
 	location ~ ^/(?<fwdport>\d+)/(?<fwdpath>.*)\$ {
