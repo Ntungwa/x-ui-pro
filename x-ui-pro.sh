a1a2992e0fc22c238e70d41084e22471699c235c
Zeno <86835361+Ntungwa@users.noreply.github.com>
2025-04-12 14:07:12 +0200

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -366,7 +366,7 @@ server {
 	}
         #XHTTP
         location ~ ^/(?<fwdport>\d+)/(.+)$ {
-                grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	        grpc_set_header Host $host;
                 grpc_pass grpc://127.0.0.1:$fwdport;
 		break;
         }
